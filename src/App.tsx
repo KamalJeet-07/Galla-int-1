8f053f9b3f3edb0745bb2c093bc0b468d54386e8
Kamaljeet-007 <kamaljeet006940@gmail.com>
2024-10-26 11:27:32 +0530

    jk

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Services from './components/Services';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import AdminPanel from './components/AdminPanel';
+
 
 function App() {
   return (
@@ -18,7 +18,7 @@ function App() {
       <Projects />
       <Contact />
       <Footer />
-      <AdminPanel />
+
     </div>
   );
 }
